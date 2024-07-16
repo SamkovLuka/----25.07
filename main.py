f0685201247d9971b92c998a339eba336ee39aed
SamkovLuka <staddycase@gmail.com>
2024-07-16 19:02:46 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,89 @@
+#1
+
+#done
+
+
+# file1 = open("file1.txt","w+")
+# file1.write("Hello world")
+# file1  = open("file1.txt","r")
+# line1 = file1.readlines()
+# print(line1)
+# file2 = open("file2.txt","w+")
+# file2.write("Goodbye world")
+# file2  = open("file2.txt","r")
+# line2 = file2.readlines()
+# print(line2)
+# if line1 == line2:
+#     print("lines are the same")
+# else:
+#     print("lines are different")
+#     print(f"line 1 = {line1}")
+#     print(f"line 2 = {line2}")
+
+#2
+
+
+# file1 = open("file1_2.txt","w+")
+# file1.write("wfwf rkmbr vb ggeg")
+# file1  = open("file1_2.txt","r")
+# line1 = file1.readlines()
+# print(line1)
+# for i in line1:
+#    i+=1
+#    print(line1.count(i))
+# file2 = open("file2_2.txt","w+")
+
+
+
+#3
+
+#done
+
+# file1 = open("file1_3.txt","a+")
+# file1.write("\nHello world")
+# file2 = open("file2_3.txt","a+")
+# with open("file1_3.txt","r")as fileStream:
+#     line1 = file1.readlines()
+# if line1:
+#     line1 = line1[:-1]
+# with open("file2_3.txt","w")as fileStream:
+#     file2.writelines(line1)
+
+
+#4
+
+
+#done
+
+
+# file = open("file.txt","a+")
+# file.write("\nHello world")
+# file = open("file.txt","r")
+# line = file.readlines()
+# for i in line:
+#     print(f"довжина рядка = {len(i)}")
+# file = open("file.txt","a+")
+# file.write("\nwifqfuoyqfhefwqwfq")
+# file = open("file.txt","r")
+# line = file.readlines()
+# for i in line:
+#     print(f"довжина рядка = {len(i)}")
+
+
+#5
+
+#almost done
+
+
+# file1_5 = open("file1_5.txt","w+")
+# file1_5.write("one two three two two")
+# word = input("Введіть слово: ")
+# n = 0
+# with open("file1_5", "r") as fileStream:
+#     for file1_5 in fileStream:
+#       n = 0
+#       div = file1_5.split()
+#       file1_5 = str()
+#       n += file1_5.count(div)
+# print(f"кількість слова - {file1_5.count(word)}")
+
